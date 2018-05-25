fb1dd7999d0d8630ad72dad2d090d602b833bd0b
Matthew Smyth <msmyth1030@gmail.com>
2018-05-25 21:34:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,9 @@ function johnLennonFacts(facts) {
 function iLoveTheBeatles(number) {
   var arr = [];
   
-  do (arr.push(`I love the Beatles!`),
-  number++);
+  do {
+    arr.push(`I love the Beatles!`),
+  number++
+  };
   while (number < 15);
 }
