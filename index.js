359b63d938dbf1bfaf9b287a9b1ce6efb2dea81b
Matthew Smyth <msmyth1030@gmail.com>
2018-05-25 21:22:59 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function theBeatlesPlay(musicians, instruments) {
+  var arr = [];
+  
+  for (let i = 0; i < 4; i++) {
+    arr.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  
+  return arr;
+}
+
+function johnLennonFacts(facts) {
+  while (i < facts.length) {
+    facts[i] = `${facts[i]} + !!!`;
+    i++
+  }
+}
